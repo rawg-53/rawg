e06e386ab9a7dd941ae392b739998a6692b2cae7
wifv <abcogenshin@gmail.com>
2023-10-19 16:37:01 +0500

    calendar done

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const key = "73d26e50a7cf40d7a5ce139d275e2bfc";
-const basicUrl = "https://api.rawg.io/api/games";
-const input = document.getElementById("search");
-const calendarElement = document.getElementById("calendar");
-const monthsElement = document.getElementById("months");
-const blocksContainer = document.getElementById("blocks-container");
-const blocks = document.getElementsByClassName("block");
 const exampleBlock = `
 <div class="block game-card">
-<img src="" alt="">
+<img src="" alt="no image or bad connection">
 <div class="bottom">
 <p></p>
 <div class="plus">
@@ -16,14 +9,29 @@ const exampleBlock = `
 </div>
 </div>
 `;
+const key = "73d26e50a7cf40d7a5ce139d275e2bfc";
+const basicUrl = "https://api.rawg.io/api/games";
+const input = document.getElementById("search");
+const calendarElement = document.getElementById("calendar");
+const monthsElement = document.getElementById("months");
+const months = document.getElementsByClassName("month");
+const blocksContainer = document.getElementById("blocks-container");
+const blocks = document.getElementsByClassName("block");
 const homePage = document.getElementById("home")
 const last30 = document.getElementById("last-month");
 const this7 = document.getElementById("this-week");
 const next7 = document.getElementById("next-week");
+const bestOfYear = document.getElementById("best-of-year");
 
 let page = 1;
 let date = new Date();
 date = date.toISOString().split('T')[0];
+
+for(let i = 0; i < months.length; i++) {
+	months[i].addEventListener("click", () => {
+		monthly(months[i].value)
+	})
+}
 
 homePage.addEventListener("click", () => {
 	home();
@@ -37,6 +45,10 @@ this7.addEventListener("click", () => {
 next7.addEventListener("click", () => {
 	nextWeek();
 });
+
+bestOfYear.addEventListener("click", () => {
+	
+})
 
 calendarElement.addEventListener("click", () => {
 	monthsElement.style.display = "flex";
@@ -97,19 +109,28 @@ async function nextWeek() {
 	const response = await fetch(`${basicUrl}?key=${key}&dates=${date},${nextWeek}&fields=announced,unanounced`);
 	const data = await response.json();
 	console.log(data);
-	repeatingLoop(data)
+	repeatingLoop(data);
 }
 
 async function monthly(month) {
-	const response = await fetch(`${basicUrl}?key=${key}&dates=`);
+	let date1 = handleMonths(month);
+	let date2 = handleMonths(month + 1)
+	let month2 = month + 1;
+	console.log(month2);
+	const response = await fetch(`${basicUrl}?key=${key}&dates=${date1},${date2}&page=${page}&ordering=-release`);
+	const data = await response.json();
+	console.log(data);
+	repeatingLoop(data);
 }
 
-function months(month) {
-	let date = new Date();
-	date.setMonth(month);
-	
+function handleMonths(month) {
+	let s = new Date()
+	s.setMonth(month)
+	s.setDate(1)
+	s = s.toISOString().split('T')[0];
+	console.log(s)
+	return s;
 }
-
 
 function repeatingLoop(data) {
 	blocksContainer.innerHTML = "";
@@ -120,8 +141,9 @@ function repeatingLoop(data) {
 		blocks[i].firstElementChild.src = data.results[i].background_image;
 		blocks[i].children[1].firstElementChild.innerText = data.results[i].name;
 	}
+	
 	let a = document.getElementsByClassName("block");
-	console.log(a)
+
 	for (let i = 0; i< a.length; i++) {
 		a[i].addEventListener("mouseenter", () => {
 			a[i].style.height = "calc(100% + 200px)"
@@ -147,17 +169,17 @@ function repeatingLoop(data) {
  * on the given sign by given Number (integer)
  */
 function days(days, sign) {
-	let date = new Date();
+	let date1 = new Date();
 	if(sign == "+") {
-		const newDate = new Date(date.getTime() + 7 * 24 * 60 * 60 * 1000);
+		const newDate = new Date(date1.getTime() + 7 * 24 * 60 * 60 * 1000);
 		return newDate.toISOString().split('T')[0];
 	} if(sign == "-") {
-		date.setDate(date.getDate() - days);
+		date1.setDate(date1.getDate() - days);
 	} else {
 		return "";
 	}
-	date = date.toISOString().split('T')[0];
-	return date;
+	date1 = date1.toISOString().split('T')[0];
+	return date1;
 }
 
 // home();	
